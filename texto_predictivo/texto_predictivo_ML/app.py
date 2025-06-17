c2cf1fa30ec492b1e99f63e5101c31a9fe8a3c9e
jcbf08 <68343304+jcbf08@users.noreply.github.com>
2025-06-17 05:06:02 +0000

    deply_final

--- texto_predictivo/texto_predictivo_ML/app.py
+++ texto_predictivo/texto_predictivo_ML/app.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import joblib
+
+# **********************
 import os
-st.write_stream(os.path)
+import streamlit as st
+import sys
+
+# Obtener el path absoluto del script actual
+try:
+    ruta_script = os.path.abspath(__file__)
+except NameError:
+    # __file__ puede no estar definido en ejecución interactiva (ej: Streamlit Cloud)
+    ruta_script = os.path.abspath(sys.argv[0])
+
+st.subheader("📍 Ruta del script ejecutado:")
+st.code(ruta_script)
+# **********************
+
+
 # Cargar modelo y vectorizador
 #model = joblib.load("modelo_predictivo.pkl")
 #vectorizer = joblib.load("vectorizador.pkl")
