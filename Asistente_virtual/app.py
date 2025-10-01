28a2b4716b16246136c4d10e0ac598a285fd643f
henryesquivelsegura <henryesquivelsegura@gmail.com>
2025-09-30 20:53:40 -0500

    Update app.py

--- Asistente_virtual/app.py
+++ Asistente_virtual/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Asistente de Voz", layout="centered")
 
-st.title("🗣️ Tu asistente de Voz Todo en Uno111")
+st.title("🗣️ Tu asistente de Voz Todo en Uno")
 st.write("Aplicación de chat habilitada por voz (GPT-4o + Micrófono)")
 
 # Capturar voz y convertirla a texto
@@ -21,4 +21,5 @@ if text:
     st.write("🧑 Tú: ", text)
     response = llm.invoke(text)
     st.write("🤖 Respuesta del modelo: ", response.content)
+
 
